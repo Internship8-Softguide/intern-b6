c14d603e44a786afdc914bdd331ece6cb2804404
Saw Theodore <sawtheodore@gmail.com>
2024-10-21 11:23:22 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$name = "John";
+$arr = ["John","Bruce",11];
+
+// define("FIRST", "bruce");
+
+// echo $name;
+// print($name);
+// echo "<br>";
+echo "<pre>";
+// echo FIRST;
+// var_dump($arr) ;
+
+
+$string = "this,is,string,a";
+
+// echo strlen($string);
+// echo str_word_count($string) ;
+// echo strrev($string);
+// echo strpos($string, "s");
+// echo substr($string, 0, 4);
+// echo strtoupper($string);
+// echo strtolower($string);
+// echo ucfirst($string);
+// echo ucwords($string);
+// echo str_replace("is", "are", $string);
+
+// echo trim($string);
+// var_dump(str_split($string, "3")) ;
+// var_dump(explode(",", $string)) ;
+
+// echo strip_tags("<h1>Hello <b>world!</b></h1>");
